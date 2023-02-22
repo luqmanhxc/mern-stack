9611004c2f1ca4679788a6d13be9a629f2501b22
Luqman Hakim <luqmanhxc@gmail.com>
2023-02-22 16:55:27 +0800

    feat: delete workout

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,21 @@
+import axios from 'axios';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext();
+
+    const handleClick = async () => {
+        const response = await axios.delete(
+            `http://localhost:4000/api/workouts/${workout._id}`
+        );
+        const json = await response.data;
+
+        if (response.status) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json });
+        }
+    };
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
@@ -10,7 +27,14 @@ const WorkoutDetails = ({ workout }) => {
                 <strong>Number of reps: </strong>
                 {workout.reps}
             </p>
-            <p>{workout.createdAt}</p>
+            <p>
+                {formatDistanceToNow(new Date(workout.createdAt), {
+                    addSuffix: true,
+                })}
+            </p>
+            <span className="material-symbols-outlined" onClick={handleClick}>
+                Delete
+            </span>
         </div>
     );
 };
