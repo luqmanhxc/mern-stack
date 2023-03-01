089cf05d019cecd5c037b21702f6e9519bfcabe4
Luqman Hakim <luqmanhxc@gmail.com>
2023-03-01 15:28:46 +0800

    feat: protected routes

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import { useAuthContext } from './useAuthContext';
+import { useWorkoutsContext } from './useWorkoutsContext';
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext();
+    const { dispatchWorkout } = useWorkoutsContext();
 
     const logout = () => {
         // remove user from storage
@@ -9,6 +11,9 @@ export const useLogout = () => {
 
         // dispatch logout action
         dispatch({ type: 'LOGOUT' });
+
+        // set workouts to null
+        dispatchWorkout({ type: 'SET_WORKOUT', payload: null });
     };
 
     return { logout };
